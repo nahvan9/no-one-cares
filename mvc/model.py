f02ef8008a51f7dbe6103cf9fda474b1565a579c
Nathan <96261577+nahvan9@users.noreply.github.com>
2022-09-27 00:34:00 -0400

    Update model.py

--- mvc/model.py
+++ mvc/model.py
@@ -1,5 +1,27 @@
 
 
 class AppModel():
-    def __init__(self):
-        pass
+    def __init__(self, input, output='', option=0):
+        self.input = input
+        self.output = output
+        self.option = option
+
+    def convertInput(self):
+        stringArray = self.input.lower().split(' ')
+        return stringArray
+
+    def option1(self, array):
+        for word in array:
+            x = len(word)
+
+
+    def checkLower(self, letter):
+        if letter == letter.lower():
+            return True
+        else:
+            return False
+        
+app = AppModel('Hello World How are You?')
+print(app.convertInput())
+print(app.checkLower('?'))
+print(app.checkLower('A'))
